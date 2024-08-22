3b14c1a9b99a37d32e5a5c9599e25213bc13cdf8
Viet An <luongvietan.231123@gmail.com>
2024-08-23 00:59:45 +0700

    First basic push : Adding routes path

---
+++ front-end/src/App.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Cart from "./pages/Cart";
+import Collection from "./pages/Collection";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+import Orders from "./pages/Orders";
+import Placeorder from "./pages/Placeorder";
+import Product from "./pages/Product";
+
+const App = () => {
+  return (
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/place-order" element={<Placeorder />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/*" element={<Home />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
