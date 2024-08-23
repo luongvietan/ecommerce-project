00b5c92fc6dccfcc12ff992b6c693f5eab083c52
Viet An <luongvietan.231123@gmail.com>
2024-08-23 12:24:50 +0700

    components/LatestCollection.jsx : setLatestProduct

--- front-end/src/components/LatestCollection.jsx
+++ front-end/src/components/LatestCollection.jsx
@@ -1,11 +1,26 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
+import Title from "./Title";
 
 const LatestCollection = () => {
   const { products } = useContext(ShopContext);
-  console.log(products);
-
-  return <div></div>;
+  const [latestProduct, setLatestProduct] = useState([]);
+  useEffect(() => {
+    setLatestProduct(products.slice(0, 10));
+  }, []);
+  return (
+    <div className="my-10">
+      <div className="text-center py-8 text-3xl">
+        <Title text1={"LASTEST"} text2={"COLLECTION"} />
+        <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis
+          eaque tenetur ad excepturi consequuntur quibusdam officiis quisquam
+          cupiditate asperiores laudantium, autem saepe cumque laboriosam minima
+          accusantium? Quod atque veniam perferendis.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default LatestCollection;
