3ce636af61ea900e7b38aed9eb107b7aa556ba89
Viet An <luongvietan.231123@gmail.com>
2024-08-23 11:09:04 +0700

    hero.jsx : Update hero left side

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Hero from "../components/Hero";
 
 const Home = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <Hero></Hero>
+    </div>
+  );
 };
 
 export default Home;
