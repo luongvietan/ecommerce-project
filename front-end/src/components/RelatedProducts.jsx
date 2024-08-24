74ca206d08f46933948443b6f7bf604b89a6305a
Viet An <luongvietan.231123@gmail.com>
2024-08-25 02:24:28 +0700

    edit RelatedProducts.jsx

--- front-end/src/components/RelatedProducts.jsx
+++ front-end/src/components/RelatedProducts.jsx
@@ -22,19 +22,16 @@ const RelatedProducts = ({ category, subCategory }) => {
     <div className="my-24">
       <div className="text-center text-3xl py-2">
         <Title text1={"RELATED"} text2={"PRODUCTS"} />
-        {console.log(`related : `, related)}
       </div>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6 ">
         {related.map((item, index) => (
-          <Link to={`/product/:${item._id}`}>
-            <ProductItem
-              key={index}
-              id={item._id}
-              name={item.name}
-              price={item.price}
-              image={item.image}
-            />
-          </Link>
+          <ProductItem
+            key={index}
+            id={item._id}
+            name={item.name}
+            price={item.price}
+            image={item.image}
+          />
         ))}
       </div>
     </div>
