1fbb1b11302afbfcb0a25611d090b7a535e6ee6a
Viet An <luongvietan.231123@gmail.com>
2024-08-25 02:40:15 +0700

    context/ShopContext.jsx : Done add to cart feature

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { assets } from "../assets/assets";
 import { ShopContext } from "../context/ShopContext";
 const Navbar = () => {
   const [visible, setVisible] = useState(false);
-  const { setShowSearch } = useContext(ShopContext);
+  const { setShowSearch, getCartCount } = useContext(ShopContext);
   return (
     <div className="flex items-center justify-between py-5 font-medium">
       <Link to="/">
@@ -54,7 +54,7 @@ const Navbar = () => {
         <Link to="/cart" className="relative">
           <img src={assets.cart_icon} alt="cart_icon" className="w-5 min-w-5" />
           <p className="absolute right-[-5px] botton-[-5px] w-5 text-center leading-4 bg-black text-white aspect-square rounded-full text-[-8px]">
-            3
+            {getCartCount()}
           </p>
         </Link>
         <img
--- front-end/src/context/ShopContext.jsx
+++ front-end/src/context/ShopContext.jsx
@@ -1,5 +1,6 @@
 import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
+import { toast } from "react-toastify";
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
@@ -10,6 +11,10 @@ const ShopContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
 
   const addToCart = async (itemId, size) => {
+    if (!size) {
+      toast.error("Please Select Product Size");
+      return;
+    }
     let cartData = structuredClone(cartItems);
     if (cartData[itemId]) {
       if (cartData[itemId][size]) {
@@ -24,9 +29,19 @@ const ShopContextProvider = (props) => {
     setCartItems(cartData);
   };
 
-  useEffect(() => {
-    console.log(cartItems);
-  }, [cartItems]);
+  const getCartCount = () => {
+    let totalCount = 0;
+    for (const items in cartItems) {
+      for (const item in cartItems[items]) {
+        try {
+          if (cartItems[items][item] > 0) {
+            totalCount += cartItems[items][item];
+          }
+        } catch (error) {}
+      }
+    }
+    return totalCount;
+  };
 
   const value = {
     products,
@@ -38,6 +53,7 @@ const ShopContextProvider = (props) => {
     setShowSearch,
     cartItems,
     addToCart,
+    getCartCount,
   };
 
   return (
