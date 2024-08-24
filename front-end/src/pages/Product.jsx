e54a45d79ad136cef0c23954c4bf0c5e8cdab840
Viet An <luongvietan.231123@gmail.com>
2024-08-24 16:13:21 +0700

    pages/Products.jsx : Display Product Information

--- front-end/src/pages/Product.jsx
+++ front-end/src/pages/Product.jsx
@@ -1,7 +1,119 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { ShopContext } from "../context/ShopContext";
+import { assets } from "../assets/assets";
 
 const Product = () => {
-  return <div>product</div>;
+  const { productId } = useParams();
+  const { products, currency } = useContext(ShopContext);
+  const [productData, setProductData] = useState(false);
+  const [image, setImage] = useState("");
+  const [size, setSize] = useState("");
+
+  const fetchProductData = async () => {
+    products.map((item) => {
+      if (item._id === productId) {
+        setProductData(item);
+        setImage(item.image[0]);
+        return null;
+      }
+    });
+  };
+  useEffect(() => {
+    fetchProductData();
+  }, [productId, products]);
+  return productData ? (
+    <div className="border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100">
+      {/* Products data */}
+      <div className="flex gap-12 sm:gap-12 flex-col sm:flex-row">
+        {/* Products images */}
+        <div className="flex-1 flex flex-col-reverse gap-3 sm:flex-row">
+          <div className="flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full">
+            {productData.image.map((item, index) => (
+              <img
+                onClick={() => setImage(item)}
+                src={item}
+                key={index}
+                className="w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer"
+              ></img>
+            ))}
+          </div>
+          <div className="w-full sm:w-[80%]">
+            <img className="w-full h-auto" src={image} alt="" />
+          </div>
+        </div>
+        {/* Products Information */}
+        <div className="flex-1">
+          <h1 className="font-medium text-2xl mt-2">{productData.name}</h1>
+          <div className="flex items-center gap-1 mt-2">
+            <img className="w-3 5" src={assets.star_icon} alt="" />
+            <img className="w-3 5" src={assets.star_icon} alt="" />
+            <img className="w-3 5" src={assets.star_icon} alt="" />
+            <img className="w-3 5" src={assets.star_icon} alt="" />
+            <img className="w-3 5" src={assets.star_dull_icon} alt="" />
+            <p className="pl-2">(153)</p>
+          </div>
+          <p className="mt-5 text-3xl font-medium">
+            {currency}
+            {productData.price}
+          </p>
+          <p className="mt-5 text-gray-500 md:w-4/5">
+            {productData.description}
+          </p>
+          <div className="flex flex-col gap-4 my-8">
+            <p>Select Size</p>
+            <div className="flex gap-2">
+              {productData.sizes.map((item, index) => (
+                <button
+                  onClick={() => setSize(item)}
+                  className={`border py-2 px-4 bg-gray-100 ${
+                    item === size ? "border-orange-300" : ""
+                  }`}
+                  key={index}
+                >
+                  {item}
+                </button>
+              ))}
+            </div>
+          </div>
+          <button className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700">
+            ADD TO CART
+          </button>
+          <hr className="mt-8 sm:w-4/5" />
+          <div className="text-sm text-gray-500 mt-5 flex flex-col gap-1">
+            <p>100% Original Product</p>
+            <p>Cash on delivery is available on this product</p>
+            <p>Easy refund and exhange policy within 7 days</p>
+          </div>
+        </div>
+      </div>
+      {/* Description and Review section */}
+      <div className="mt-20">
+        <div className="flex">
+          <b className="border px-5 py-3 text-sm">Description</b>
+          <p className="border px-5 py-3 text-sm">Reviews (86)</p>
+        </div>
+        <div className="flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500">
+          <p>
+            Fuble is your go-to online store for high-quality soccer gear.
+            Whether you're a seasoned pro or just starting out, we offer a
+            curated selection of jerseys, boots, and accessories to meet all
+            your football needs. At Fuble, we combine style, performance, and
+            affordability, making sure you have everything you need to perform
+            your best on the field.
+          </p>
+          <p>
+            From jerseys to boots, we offer everything you need to excel on the
+            field. Shop our carefully selected products designed for both
+            performance and style, all at competitive prices.
+          </p>
+        </div>
+      </div>
+      {/* Display related products */}
+    </div>
+  ) : (
+    <div className="opacity-0"></div>
+  );
 };
 
 export default Product;
