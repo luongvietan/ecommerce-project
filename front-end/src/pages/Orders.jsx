f4e91f497f967eafed442a77094887c6b6796024
Viet An <luongvietan.231123@gmail.com>
2024-08-25 15:28:10 +0700

    pages/Orders.jsx : Update styles Orders page

--- front-end/src/pages/Orders.jsx
+++ front-end/src/pages/Orders.jsx
@@ -19,7 +19,7 @@ const Orders = () => {
               <img className="w-16 sm:w-20" src={item.image[0]} alt="" />
               <div>
                 <p className="sm:text-base font-medium">{item.name}</p>
-                <div className="flex items-center gap-3 mt-2 text-balance text-gray-700">
+                <div className="flex items-center gap-3 mt-2 text-base text-gray-700">
                   <p className="text-lg">
                     {currency}
                     {item.price}
@@ -31,15 +31,15 @@ const Orders = () => {
                   Date: <span className="text-gray-400">25, July, 2024</span>
                 </p>
               </div>
-              <div className="md:w-1/2 flex justify-between">
-                <div className="flex items-center gap-2">
-                  <p className="min-w-2 h-2 rounded-full bg-green-500"></p>
-                  <p className="text-sm md:text-base">Ready to ship</p>
-                </div>
-                <button className="border px-4 py-2 text-sm font-medium rounded-sm">
-                  Track Order
-                </button>
+            </div>
+            <div className="md:w-1/2 flex justify-between">
+              <div className="flex items-center gap-2">
+                <p className="min-w-2 h-2 rounded-full bg-green-500"></p>
+                <p className="text-sm md:text-base">Ready to ship</p>
               </div>
+              <button className="border px-4 py-2 text-sm font-medium rounded-sm ">
+                Track Order
+              </button>
             </div>
           </div>
         ))}
