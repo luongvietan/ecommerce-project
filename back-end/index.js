648c4c5b138d85f9534b2db00bee0f30f68046d7
Viet An <luongvietan.231123@gmail.com>
2024-08-27 16:41:03 +0700

    test node running

--- back-end/index.js
+++ back-end/index.js
@@ -6,6 +6,27 @@ const jwt = require("jsonwebtoken");
 const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
+const { error } = require("console");
 
 app.use(express.json());
 app.use(cors());
+
+// Mongodb database connection
+
+mongoose.connect(
+  "mongodb+srv://luongvietan:231123@cluster0.jyd9e.mongodb.net/e-commerce"
+);
+
+// API Creation
+
+app.get("/", (request, response) => {
+  response.send("Express App is running");
+});
+
+app.listen(port, () => {
+  if (!error) {
+    console.log("Server running on port : ", port);
+  } else {
+    console.log(`Error : `, error);
+  }
+});
